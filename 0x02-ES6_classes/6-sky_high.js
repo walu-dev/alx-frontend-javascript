fd336a0f4e5438b53aab4b3d59d7888f2ded6d88
walu-dev <jonathanenoch11@gmail.com>
2023-09-28 16:02:31 -0700

    fin

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
